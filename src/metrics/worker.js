00298179e68e5842e15f2c48f60ce01ac18c6805
Daniel Larsen <dalars@microsoft.com>
2018-10-26 12:04:24 +1300

    validate URL, track and redirect

--- src/metrics/worker.js
+++ src/metrics/worker.js
@@ -1,21 +1,50 @@
+import "applicationinsights-js";
+
+// init Application insights
+var snippet = {
+    config: {
+        instrumentationKey: "393b3c89-feb0-4a30-be29-899686aaae90"
+    }
+};
+var init = new Microsoft.ApplicationInsights.Initialization(snippet);
+var appInsights = init.loadAppInsights();
+
 addEventListener('fetch', event => {
-    event.respondWith(handle(event.request))
+    event.respondWith(handle(event))
 })
 
-const _ = require("applicationinsights-js");
+async function handle(event) {
+    var request = event.request;
+    // https://msimpson.co.nz/podcasts/se104.mp3 ->
+    // https://azurelunchnz.azureedge.net/podcasts/se104.mp3
 
-var snippet = {   
-    config: {   
-        instrumentationKey: "393b3c89-feb0-4a30-be29-899686aaae90"   
-    }   
-};   
-var init = new Microsoft.ApplicationInsights.Initialization(snippet);   
-var appInsights = init.loadAppInsights();   
-appInsights.trackPageView();   
-
-async function handleRequest(request) {
     console.log('Got request', request)
-    appInsights.trackPageView();  
-  const modifiedRequest = new Request('https://msimpson.co.nz');
-  return fetch(modifiedRequest);
-  }
+    appInsights.trackPageView();
+    //const modifiedRequest = new Request('https://msimpson.co.nz');
+    //return fetch(modifiedRequest);
+
+    // validate URL
+    if (!url.startsWith('https://msimpson.co.nz/podcasts/')) {
+        event.respondWith(
+            new Response.error()
+        );
+        return;
+    }
+
+    // get and validate episode
+    var url = request.url.replace('https://msimpson.co.nz/podcasts/', 'https://azurelunchnz.azureedge.net/podcasts/')
+    var episode = url.replace('https://azurelunchnz.azureedge.net/podcasts/', '');
+    if (episode.length == 0) {
+        event.respondWith(
+            new Response.error()
+        );
+        return;
+    }
+
+    // track the download
+    appInsights.trackEvent("podcasts/download");
+    appInsights.trackEvent("podcasts/download/" + episode);
+
+    return Response.redirect(url, 302);
+
+}
