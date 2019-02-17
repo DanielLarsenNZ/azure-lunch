5e890256c16e957be19b37b13708e276499c74bb
Matt Simpson <masim@microsoft.com>
2019-02-18 10:30:43 +1300

    worker update

--- src/metrics/worker.js
+++ src/metrics/worker.js
@@ -1,15 +1,3 @@
-import {AppInsights} from 'applicationinsights-js';
-
-// init Application insights
-// var snippet = {
-//     config: {
-//         instrumentationKey: "393b3c89-feb0-4a30-be29-899686aaae90"
-//     }
-// };
-AppInsights.Setup("393b3c89-feb0-4a30-be29-899686aaae90");
-//ar init = new Microsoft.ApplicationInsights.Initialization(snippet);
-// var appInsights = init.loadAppInsights();
-
 addEventListener('fetch', event => {
     event.respondWith(handle(event))
 })
@@ -20,20 +8,6 @@ async function handle(event) {
     // https://azurelunchnz.azureedge.net/podcasts/se104.mp3
 
     console.log('Got request', request)
-    AppInsights.trackPageView();
-    //const modifiedRequest = new Request('https://msimpson.co.nz');
-    //return fetch(modifiedRequest);
-
-    // validate URL
-    
-    if (!request.url.startsWith('https://msimpson.co.nz/podcasts/')) {
-        console.log("Not a valid URL - /podcasts/ not found");
-        return Response.error();
-        // event.respondWith(
-        //     new Response.error()
-        // );
-        // return;
-    }
 
     // get and validate episode
     var url = request.url.replace('https://msimpson.co.nz/podcasts/', 'https://azurelunchnz.azureedge.net/podcasts/');
@@ -41,26 +15,28 @@ async function handle(event) {
     if (episode.length == 0) {
         console.log("Not a valid URL - No episode name found")
         return Response.error();
-        // event.respondWith(
-        //     new Response.error()
-        // );
-        // return;
     }
 
-    // track the download
-    AppInsights.trackEvent("podcasts/download");
-    AppInsights.trackEvent("podcasts/download/" + episode);
+    track(event)
 
     return Response.redirect(url, 302);
 
 }
 
-function
-hr = new XMLHttpRequest();
-hr.onreadystatechange = function() {
-    if (hr.readyState == XMLHttpRequest.DONE) {
-    doSomething(hr.responseText);
-}}
-hr.open('GET', 'https://api.applicationinsights.io/v1/apps/DEMO_APP/metrics/requests/duration?interval=PT1H', true);
-hr.setRequestHeader("x-api-key", "DEMO_KEY");
-hr.send();
+async function track(event) {
+    var body = {
+
+
+        "PartitionKey": "mypartitionkey",
+        "RowKey": "myrowkey"
+    }
+    try {
+        response = await fetch('https://azurelunchaue.table.core.windows.net/metrics', {
+            method: 'POST',
+            headers: {"Authorization": "", "x-ms-date": Date.UTC(), },
+            body: JSON.stringify(event),
+        });
+    } catch (error) {
+        console.log("Error - unable to post to table storage")
+    }
+}
